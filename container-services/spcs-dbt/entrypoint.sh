147c19964483610e89a7a3d4eea9f1ff533c19a1
eplata <domfp13@me.com>
2024-01-14 15:08:13 +0100

    Add Dockerfile and Makefile for dbt container services

---
+++ container-services/spcs-dbt/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# print out a message
+echo "Hello, world!"
+
+# Keep the script running indefinitely (use for debugging or when task services is in public preview)
+tail -f /dev/null
